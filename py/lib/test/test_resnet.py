3a07c7d551a1906cb60dc603a5c2466b48d65b93
zhujian <505169307@qq.com>
2020-04-28 21:22:57 +0800

    test(model): resnet

---
+++ py/lib/test/test_resnet.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/4/28 下午9:07
+@file: test_resnet.py
+@author: zj
+@description: 
+"""
+
+import torch
+from models.resnet import res_net
+
+
+def test_resnet18():
+    N = 1
+    num_classes = 20
+
+    data = torch.randn((N, 3, 224, 224))
+    model = res_net.resnet18(num_classes=num_classes)
+    outputs = model(data)
+
+    assert outputs.shape == (N, num_classes)
+
+
+def test_resnet34():
+    N = 1
+    num_classes = 20
+
+    data = torch.randn((N, 3, 224, 224))
+    model = res_net.resnet34(num_classes=num_classes)
+    outputs = model(data)
+
+    assert outputs.shape == (N, num_classes)
+
+
+def test_resnet50():
+    N = 1
+    num_classes = 20
+
+    data = torch.randn((N, 3, 224, 224))
+    model = res_net.resnet50(num_classes=num_classes)
+    outputs = model(data)
+
+    assert outputs.shape == (N, num_classes)
+
+
+def test_resnet101():
+    N = 1
+    num_classes = 20
+
+    data = torch.randn((N, 3, 224, 224))
+    model = res_net.resnet101(num_classes=num_classes)
+    outputs = model(data)
+
+    assert outputs.shape == (N, num_classes)
+
+
+def test_resnet152():
+    N = 1
+    num_classes = 20
+
+    data = torch.randn((N, 3, 224, 224))
+    model = res_net.resnet152(num_classes=num_classes)
+    outputs = model(data)
+
+    assert outputs.shape == (N, num_classes)
